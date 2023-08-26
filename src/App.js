8654b3e3460995eb5e5955eb9974c095ed908e0a
Karan Singh <44173143+karan-ksrk@users.noreply.github.com>
2023-08-26 04:19:56 +0000

    update bio-link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import medium from './assets/medium.png';
 import website from './assets/website.png';
 import { useRive, useStateMachineInput } from 'rive-react';
 import character from './assets/character.riv';
-
+import React from "react";
 const STATE_MACHINE_NAME = 'State Machine 1';
 
 function App() {
@@ -63,6 +63,23 @@ function App() {
           </button>
         </a>
       </div>
+      <a href="https://dev.codebeaker.xyz" target="_blank" rel="noopener">
+        <div className="my-2 flex h-16 items-center justify-between rounded-lg border-2 border-b-4 border-l-4 border-black px-4 shadow-xl">
+          <div className="flex items-center">
+            <img alt="photo" className="w-20 rounded-full" src={website} />
+            <div className="ml-2">
+              <div className="text-xs font-semibold">Check out my Website</div>
+              <div className="flex text-xs font-light text-gray-600">
+              dev.codebeaker.xyz<svg className="h-3 w-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+                  <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </a>
 
       <a href="https://www.youtube.com/channel/UC3jlv75GpZ8v-Hk-2JyzryQ" target="_blank" rel="noopener">
         <div className="my-2 flex h-16 items-center justify-between rounded-lg border-2 border-b-4 border-l-4 border-black px-4 shadow-xl">
@@ -100,23 +117,7 @@ function App() {
         </div>
       </a>
 
-      <a href="https://ksrk47.xyz/token-minter//" target="_blank" rel="noopener">
-        <div className="my-2 flex h-16 items-center justify-between rounded-lg border-2 border-b-4 border-l-4 border-black px-4 shadow-xl">
-          <div className="flex items-center">
-            <img alt="photo" className="w-10 rounded-full" src="https://www.pngall.com/wp-content/uploads/10/Solana-Crypto-Logo-PNG-File.png" />
-            <div className="ml-2">
-              <div className="text-xs font-semibold">Current Project</div>
-              <div className="flex text-xs font-light text-gray-600">
-                Solana token-minter<svg className="h-3 w-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
-                  <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div></div>
-        </div>
-      </a>
+      
 
       <div className="text-center font-extrabold text-black">&copy; karan-ksrk</div>
     </div>
